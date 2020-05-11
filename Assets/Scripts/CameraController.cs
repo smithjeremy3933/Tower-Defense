dc138455e4a28ccdc355feac66e6b6f93483cc31
smithjeremy3933@gmail.com <smithjeremy3933@gmail.com>
2020-05-11 13:27:35 -0700

    Got AStar path integrated, now working on getting enemies to move on the path to the end node

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    float xSpeed = 4f;
+    float ySpeed = 4f;
+    float xRange = 6f;
+
+    void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float xThrow = Input.GetAxis("Horizontal");
+        float yThrow = Input.GetAxis("Vertical");
+
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        float yOffset = yThrow * ySpeed * Time.deltaTime;
+
+        float rawNewXPos = transform.position.x + xOffset;
+        float rawNewYPos = transform.position.z + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawNewXPos, -xRange, xRange);
+
+        transform.position = new Vector3(clampedXPos, transform.position.y, rawNewYPos);
+    }
+
+}
--- Assets/Scripts/TileController.cs
+++ Assets/Scripts/TileController.cs
@@ -7,6 +7,12 @@ public class TileController : MonoBehaviour
     public MapData mapData;
     public Graph graph;
 
+    public Pathfinder pathfinder;
+    public int startX = 0;
+    public int startY = 0;
+    public int goalX = 6;
+    public int goalY = 5;
+
     void Start()
     {
         if (mapData != null && graph != null)
@@ -19,6 +25,14 @@ public class TileController : MonoBehaviour
             if (graphView != null)
             {
                 graphView.Init(graph);
+            }
+
+            if (graph.IsWithinBounds(startX, startY) && graph.IsWithinBounds(goalX, goalY) && pathfinder != null)
+            {
+                Node startNode = graph.nodes[startX, startY];
+                Node goalNode = graph.nodes[goalX, goalY];
+                pathfinder.Init(graph, graphView, startNode, goalNode);
+                pathfinder.SearchRoutine();
             }
         }
     }
