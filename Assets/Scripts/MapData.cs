2397aef750aa7b99a326c32acb49f089561059f8
smithjeremy3933@gmail.com <smithjeremy3933@gmail.com>
2020-05-10 18:21:22 -0700

    Got tile system working, next to work on path for enemies

---
+++ Assets/Scripts/MapData.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapData : MonoBehaviour
+{
+    public int width = 15;
+    public int height = 15;
+
+    public int[,] MakeMap()
+    {
+        int[,] map = new int[width, height];
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                map[x, y] = 0;               
+            }
+        }
+
+        return map;
+    }
+
+}
