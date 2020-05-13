2f8433a1a1539cff41689044950b3ebaaf785a4a
smithjeremy3933@gmail.com <smithjeremy3933@gmail.com>
2020-05-13 12:31:54 -0700

    Made full game loop with temporary start and end screens. Added Postprocessing Manager from Unity. Need to work on spawning and moving animations using Itween from Unity.

---
+++ Assets/Scripts/EndScreen.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class EndScreen : MonoBehaviour
+{
+    public PostProcessProfile blurProfile;
+    public PostProcessProfile normalProfile;
+    public PostProcessVolume postProcessVolume;
+
+    public void EnableCameraBlur(bool state)
+    {
+        if (postProcessVolume != null && blurProfile != null && normalProfile != null)
+        {
+            if (state)
+            {
+                postProcessVolume.profile = blurProfile;
+            }
+            else
+            {
+                postProcessVolume.profile = normalProfile;
+            }
+        }
+    }
+}
